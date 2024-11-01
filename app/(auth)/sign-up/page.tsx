c69bd37d5beaed9641051fba30b82d7c7562e0f3
Zal Hazmi Bin Musrizal <zalhazmi.musrizal@student.gmi.edu.my>
2024-11-01 21:09:56 +0800

    Sign in and Sign Up UI finished

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='flex-center size-fill max-sm:px-6'>
+            <AuthForm type="sign-up"/>
+        </section>
+    )
 }
 
 export default SignUp
