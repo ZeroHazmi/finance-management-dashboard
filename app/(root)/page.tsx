cf43b4096df368fc4ef0a658b4007e4fac49bf61
Zal Hazmi Bin Musrizal <zalhazmi.musrizal@student.gmi.edu.my>
2024-11-01 19:13:16 +0800

    Finished Home Page along with Sidebar and RightSideBar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,40 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-	const loggedIn = { firstName : "Zal Hazmi"}
-	return (
-	<section className='home'>
-		<div className='home-content'>
-			<header className='home-header'>
-				<HeaderBox 
-					type="greeting"
-					title="Welcome"
-					user = {loggedIn?.firstName || "Guest"}
-					subtext='Access and manage your account and transction efficiently.'
-				/>
+    const loggedIn = { firstName : "Zal Hazmi" , lastName : "Musrizal" , email : "zerohazmi@gmail.com"}
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user = {loggedIn?.firstName || "Guest"}
+                        subtext='Access and manage your account and transction efficiently.'
+                    />
 
-				<TotalBalanceBox 
-					accounts={[]}
-					totalBanks={1}
-					totalCurrentBalance={1250.35}
-				/>
-			</header>
-		</div>
-	</section>
-	)
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+                RECENT TRANSACTION
+
+            </div>
+
+            <RightSideBar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{currentBalance: 1200}, {currentBalance: 1200}]}
+            />
+        </section>
+    )
 }
 
 export default Home
